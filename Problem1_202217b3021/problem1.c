654acece4e87d8345b70f3ec97a7cfa980018a42
202217b3021 <202217b3021@wilp.bits-pilani.ac.in>
2024-07-13 07:20:51 +0000

    problem1.c

--- Problem1_202217b3021/problem1.c
+++ Problem1_202217b3021/problem1.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-#include<conio.h>
 #include<stdlib.h>
 
 #define MAX 10
 
-/* Define the required functions to create a list, insert into the list, 
+/* Define the required functions to create a list, insert into the list,
 delete an element from the list, search and display the list */
 
 void create();
@@ -24,12 +23,12 @@ void main()
 	{
 		printf("\n main Menu");
 		printf("\n 1.Create \n 2.Delete \n 3.Search \n 4.Insert \n 5.Display\n 6.Exit \n");
-		printf("\n Enter your Choice");
+		printf("\n Enter your Choice ");
 		scanf("%d", &ch);
 
-		/* The following switch will call the appropriate choice provided 
+		/* The following switch will call the appropriate choice provided
 		by the user */
-		
+
 		switch(ch)
 		{
 			case 1:
@@ -53,63 +52,63 @@ void main()
 			default:
 				printf(" \n Enter the correct choice:");
 		}
-		printf("\n Do u want to continue:::");
+		printf("\n Do u want to continue : : : ");
 		scanf("\n%c", &g);
 	}
 
-	/* The program is intended to run till we provide inputs other that ‘y’ 
+	/* The program is intended to run till we provide inputs other that ‘y’
 	or ‘Y’ */
 	while(g=='y'||g=='Y');
-		getch(); 
+		getchar();
 }
 
-void create() 
+void create()
 {
-	printf("\n Enter the number of nodes");
+	printf("\n Enter the number of nodes ");
 	scanf("%d", &n);
 
 	/* The loop should run till we get the ‘n’ inputs */
-	for(i=0;i<n;i++) 
+	for(i=0;i<n;i++)
 	{
-		printf("\n Enter the Element:",i+1);
-		scanf("%d", &b[i]); 
+		printf("\n Enter the Element : ",i+1);
+		scanf("%d", &b[i]);
 	}
 }
 
-void deletion() 
+void deletion()
 {
-	printf("\n Enter the position u want to delete::");
+	printf("\n Enter the position u want to delete : : ");
 	scanf("%d", &pos);
-	
-	if(pos>=n) 
+
+	if(pos>=n)
 	{
-		printf("\n Invalid Location::");
+		printf("\n Invalid Location : : ");
 	}
-	else 
+	else
 	{
-		for(i=pos+1;i<n;i++) 
+		for(i=pos+1;i<n;i++)
 		{
 			b[i-1]=b[i];
 		}n--;
 	}
-	printf("\n The Elements after deletion");
-	for(i=0;i<n;i++) 
+	printf("\n The Elements after deletion ");
+	for(i=0;i<n;i++)
 	{
 		printf("\t%d", b[i]);
 	}
 }
 
-void search() 
+void search()
 {
-	printf("\n Enter the Element to be searched:");
+	printf("\n Enter the Element to be searched : ");
 	scanf("%d", &e);
-	for(i=0;i<n;i++) 
+	for(i=0;i<n;i++)
 	{
-		if(b[i]==e) 
+		if(b[i]==e)
 		{
-			printf("Value is in the %d Position", i);
+			printf("Value is in the %d Position ", i);
 		}
-		else 
+		else
 		{
 			printf("Value %d is not in the list::", e);
 			continue;
@@ -117,33 +116,33 @@ void search()
 	}
 }
 
-void insert() 
+void insert()
 {
 	printf("\n Enter the position u need to insert::");
 	scanf("%d", &pos);
-	if(pos>=n) 
+	if(pos>=n)
 	{
-		printf("\n invalid Location::"); 
+		printf("\n invalid Location::");
 	}
 	else
 	{
 		for(i=MAX-1;i>=pos-1;i--)
 		{
-			b[i+1]=b[i]; 
+			b[i+1]=b[i];
 		}
 		printf("\n Enter the element to insert::\n");
 		scanf("%d",&p);
 		b[pos]=p;
-		n++; 
+		n++;
 	}
 	printf("\n The list after insertion::\n");
-	display(); 
+	display();
 }
 
-void display() 
+void display()
 {
 	printf("\n The Elements of The list ADT are:");
-	for(i=0;i<n;i++) 
+	for(i=0;i<n;i++)
 	{
 		printf("\n\n%d", b[i]);
 	}
